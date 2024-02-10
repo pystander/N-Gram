0dfd83b27ee29ad2ad731b9d830cb9550534fd93
Rain Leung <rainleung0218@gmail.com>
2024-02-10 19:59:33 +0800

    Add add-k smoothed n-gram model

--- ngram.py
+++ ngram.py
@@ -49,9 +49,6 @@ class NGram:
         self.counter = counter
 
     def get_log_prob(self, token_set: tuple) -> float:
-        # Check if trained
-        assert self.counter != None
-
         # Compute probability
         counter = self.counter
         subset = tuple(token_set[:-1])
@@ -76,9 +73,80 @@ class NGram:
                 token_set = tuple(tokens[i : i + n])
 
                 if token_set in counter:
-                    subset = tuple(token_set[:-1])
                     log_sum += self.get_log_prob(token_set)
                 else:
                     return float("inf")
+
+        return pow(2, (-1 / N) * log_sum)
+
+
+class SmoothedNGram(NGram):
+    def __init__(self, n: int, vocab: set) -> None:
+        super().__init__(n, vocab)
+
+        self.V = len(vocab)
+
+    # Override
+    def fit(self, train_lines: list[str]) -> None:
+        # Count n-gram and (n-1)-gram
+        n = self.n
+        counter = {}
+
+        for line in train_lines:
+            tokens = self.tokenize(line)
+
+            for i in range(len(tokens) - n + 1):
+                token_set = tuple(tokens[i : i + n])
+                subset = tuple(token_set[:-1])
+
+                if token_set in counter:
+                    counter[token_set] += 1
+                else:
+                    counter[token_set] = 1
+
+                if subset in counter:
+                    counter[subset] += 1
+                else:
+                    counter[subset] = 1
+
+        self.counter = counter
+
+    # Override
+    def get_log_prob(self, token_set: tuple, k: float = 0) -> float:
+        # Check if trained
+        assert self.counter != None
+
+        # Compute probability
+        counter = self.counter
+        V = self.V
+        subset = tuple(token_set[:-1])
+
+        if token_set in counter:
+            return math.log2((counter[token_set] + k) / (counter[subset] + k * V))
+        else:
+            subset_count = 0
+
+            if subset in counter:
+                subset_count = counter[subset]
+
+            return math.log2(k / (subset_count + k * V))
+
+    # Override
+    def get_perplexity(self, test_lines: list[str], k: float) -> float:
+        # Check if trained
+        assert self.counter != None
+
+        # Compute product (log sum) of probability
+        n = self.n
+        log_sum = 0
+        N = 0
+
+        for line in test_lines:
+            tokens = self.tokenize(line)
+            N += len(tokens)
+
+            for i in range(len(tokens) - n + 1):
+                token_set = tuple(tokens[i : i + n])
+                log_sum += self.get_log_prob(token_set, k)
 
         return pow(2, (-1 / N) * log_sum)
